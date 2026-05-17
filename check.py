a070f6d76aac479d1f3f22c65c2b92d7a8fa1b41
Nithish <nithish221b@gmail.com>
2026-05-17 22:36:05 +0530

    wav and srt commit

---
+++ check.py
@@ -0,0 +1,2 @@
+from llama_cpp import llama_cpp
+print(llama_cpp.llama_print_system_info())
--- subtitles_generation.py
+++ subtitles_generation.py
@@ -1,42 +1,43 @@
 from faster_whisper import WhisperModel
 
-model = WhisperModel(
-    "base",
-    device="cuda",
-    compute_type="float16"
-)
-
-segments, info = model.transcribe(
-    r"C:\Users\Nithish\Content_Automisation\audio\output.wav",
-    beam_size=5
-)
-
-def format_time(seconds):
-    hours = int(seconds // 3600)
-    minutes = int((seconds % 3600) // 60)
-    secs = int(seconds % 60)
-    millis = int((seconds - int(seconds)) * 1000)
-
-    return (
-        f"{hours:02}:{minutes:02}:"
-        f"{secs:02},{millis:03}"
+def subtitles_generation():
+    model = WhisperModel(
+        "base",
+        device="cuda",
+        compute_type="float16"
     )
 
-with open(
-    r"C:\Users\Nithish\Content_Automisation\audio\subtitles.srt",
-    "w",
-    encoding="utf-8"
-) as f:
+    segments, info = model.transcribe(
+        r"C:\Users\Nithish\Content_Automisation\audio\output.wav",
+        beam_size=5
+    )
 
-    for i, segment in enumerate(segments, start=1):
+    def format_time(seconds):
+        hours = int(seconds // 3600)
+        minutes = int((seconds % 3600) // 60)
+        secs = int(seconds % 60)
+        millis = int((seconds - int(seconds)) * 1000)
 
-        f.write(f"{i}\n")
-
-        f.write(
-            f"{format_time(segment.start)} --> "
-            f"{format_time(segment.end)}\n"
+        return (
+            f"{hours:02}:{minutes:02}:"
+            f"{secs:02},{millis:03}"
         )
 
-        f.write(segment.text.strip() + "\n\n")
+    with open(
+        r"C:\Users\Nithish\Content_Automisation\audio\subtitles.srt",
+        "w",
+        encoding="utf-8"
+    ) as f:
 
-print("SRT generated successfully!")
+        for i, segment in enumerate(segments, start=1):
+
+            f.write(f"{i}\n")
+
+            f.write(
+             f"{format_time(segment.start)} --> "
+                f"{format_time(segment.end)}\n"
+            )
+
+            f.write(segment.text.strip() + "\n\n")
+
+    print("SRT generated successfully!")
